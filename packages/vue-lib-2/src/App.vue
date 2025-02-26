521813c67e65184067f8b1a9e2edc90b5b65ab38
Rafael Moret <rafaelmoretgalan@gmail.com>
2025-02-26 21:14:52 +0100

    feat: init second vue package

--- packages/vue-lib-2/src/App.vue
+++ packages/vue-lib-2/src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Vue Lib 1!" />
+      <HelloWorld msg="Vue Lib 2!" />
     </div>
   </header>
 
